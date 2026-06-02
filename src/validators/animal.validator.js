fcf5a72255072bc3f9390081475c7261e79561c1
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-06-02 11:10:58 +0200

    Mmodification validation donnée

--- src/validators/animal.validator.js
+++ src/validators/animal.validator.js
@@ -2,20 +2,21 @@ import { body, param } from 'express-validator';
 
 export const validateAnimal = [
   body('name')
+    .trim() //supprime les espaces inutiles début et fin
+    .notEmpty()
+    .withMessage('Le nom est requis')
     .matches(/^[a-zA-ZàâäéèêëîïôöùûüçÉÀÂÄÈÊËÎÏÔÖÙÛÜÇ\s-]+$/) //lettres min et maj, les accents, les tirets (-) et les espaces (\s)
     .withMessage(
       'Le nom ne doit contenir que des lettres, des tirets ou des espaces'
     )
     .isLength({ min: 2, max: 50 })
-    .withMessage('Le nom doit contenir au minimum 2 à 50 caractères')
-    .notEmpty()
-    .withMessage('Le nom est requis'),
+    .withMessage('Le nom doit contenir au minimum 2 à 50 caractères'),
 
   body('gender')
-    .isIn(['male', 'female'])
-    .withMessage('Le genre doit être masculin ou féminin') //isIN() permet de vérifier que la donnée fait partie d'une liste précise de valeurs autorisées
     .notEmpty()
-    .withMessage('Le genre est requis'),
+    .withMessage('Le genre est requis')
+    .isIn(['male', 'female'])
+    .withMessage('Le genre doit être masculin ou féminin'), //isIN() permet de vérifier que la donnée fait partie d'une liste précise de valeurs autorisées
 
   body('age')
     .isInt({ min: 0 })
@@ -24,38 +25,42 @@ export const validateAnimal = [
     .withMessage("L'age est requis"),
 
   body('size')
-    .isIn(['small', 'medium', 'big'])
-    .withMessage('La taille doit être grand, moyen ou petit')
     .notEmpty()
-    .withMessage('La taille est requise'),
+    .withMessage('La taille est requise')
+    .isIn(['small', 'medium', 'big'])
+    .withMessage('La taille doit être grand, moyen ou petit'),
 
   body('description')
-    .isLength({ min: 10, max: 500 })
-    .withMessage('La description doit contenir au minimum 10 à 500 caractères')
+    .trim()
     .notEmpty()
-    .withMessage('La description est requise'),
+    .withMessage('La description est requise')
+    .isLength({ min: 10, max: 500 })
+    .withMessage('La description doit contenir au minimum 10 à 500 caractères'),
 
   body('status')
-    .isIn(['available', 'in_progress', 'adopted'])
-    .withMessage('Le status doit être disponible, en cours ou adopté')
     .notEmpty()
-    .withMessage('Le status est requis'),
+    .withMessage('Le status est requis')
+    .isIn(['available', 'in_progress', 'adopted'])
+    .withMessage('Le status doit être disponible, en cours ou adopté'),
 
   body('id_breed')
+    .notEmpty()
+    .withMessage('La race est requise')
     .isInt({ min: 1 })
     .withMessage('La race sélectionnée n’est pas valide')
-    .notEmpty()
-    .withMessage('La race est requise'),
+    .toInt(), //converti "1" en 1
 
   body('specie')
+    .notEmpty()
+    .withMessage("L'espèce est requise")
     .isInt({ min: 1 })
     .withMessage("L'espèce sélectionnée n’est pas valide")
-    .notEmpty()
-    .withMessage("L'espèce est requise"),
+    .toInt(),
 ];
 
 export const validateAnimalId = [
   param('id')
     .isInt({ min: 1 })
-    .withMessage("L'id n'est pas un nombre entier positif"),
+    .withMessage("L'id n'est pas un nombre entier positif")
+    .toInt(),
 ];
