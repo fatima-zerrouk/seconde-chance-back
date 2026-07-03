915f6241a0c080eb8d37d2fe3075855be655041f
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-07-03 13:47:49 +0200

    Formatage du code

--- src/services/animal.services.js
+++ src/services/animal.services.js
@@ -2,29 +2,31 @@ import * as AnimalModel from '../model/animal.model.js';
 import AppError from '../errors/AppError.js';
 
 // Création de l'animal
-export const createAnimal = async (data) => {
+export const createAnimal = async data => {
   const id = await AnimalModel.createWithPicture(data);
   const animal = await AnimalModel.findById(id);
-  
+
   if (!animal) {
-    throw new AppError("Erreur lors de la création de l'animal en base de données", 500);
+    throw new AppError(
+      "Erreur lors de la création de l'animal en base de données",
+      500
+    );
   }
   return animal;
 };
- 
+
 // Récupération d'un animal
-export const getAnimalById = async (id) => {
-  
+export const getAnimalById = async id => {
   const animal = await AnimalModel.findById(id);
-  
-  // Si le modèle renvoie null, c'est que l'ID n'existe pas 
+
+  // Si le modèle renvoie null, c'est que l'ID n'existe pas
   if (!animal) {
     throw new AppError("Cet animal n'existe pas ou a été supprimé", 404);
   }
   return animal;
 };
 
-// Modification d'un animal 
+// Modification d'un animal
 export const updateAnimal = async (id, data) => {
   // Vérifie si l'animal existe avant de lancer l'update SQL
   const animalExists = await AnimalModel.findById(id);
@@ -34,7 +36,7 @@ export const updateAnimal = async (id, data) => {
 
   // Lance la mise à jour (qui utilise la transaction SQL)
   await AnimalModel.updateWithPicture(id, data);
-  
+
   // Récupère et renvoie l'animal mis à jour
   return await AnimalModel.findById(id);
 };
